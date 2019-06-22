d292bedb4b018c377fa6eaadc7f2e5843ba0e836
Ignacio Esposto <ignaciospst@gmail.com>
2019-06-22 14:04:09 -0300

    test notificaciones a mano

--- js/app.js
+++ js/app.js
@@ -192,3 +192,65 @@ setTimeout(function() {
 
 
 
+
+
+
+
+
+
+function enviarNotificacion() {
+
+    const notificationOpts = {
+        body: 'Este es el cuerpo de la notificación',
+        icon: 'img/icons/icon-72x72.png'
+    };
+
+
+    const n = new Notification('Hola Mundo', notificationOpts);
+
+    n.onclick = () => {
+        console.log('Click');
+    };
+
+}
+
+
+function notificarme() {
+
+    if ( !window.Notification ) {
+        console.log('Este navegador no soporta notificaciones');
+        return;
+    }
+
+    if ( Notification.permission === 'granted' ) {
+        
+        // new Notification('Hola Mundo! - granted');
+        enviarNotificacion();
+
+    } else if ( Notification.permission !== 'denied' || Notification.permission === 'default' )  {
+
+        Notification.requestPermission( function( permission ) {
+
+            console.log( permission );
+
+            if ( permission === 'granted' ) {
+                // new Notification('Hola Mundo! - pregunta');
+                enviarNotificacion();
+            }
+
+        });
+
+    }
+
+
+
+}
+
+
+setTimeout(function() {
+    notificarme();
+}, 3000);
+
+setTimeout(function() {
+    notificarme();
+}, 10000);
